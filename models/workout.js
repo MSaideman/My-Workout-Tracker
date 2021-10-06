d45ef893f2424b7e6dc8edbf02f051c31cdb9798
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-05 21:35:42 -0700

    added model info based on seeds

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,20 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+// model info
+
+// const Class = new Schema({
+// day: {},
+// exercises:[
+// {type:},
+// {name:},
+// {duration:},
+// {weight:},
+// {reps:},
+// {sets:},
+// {distance:}
+// ]
+// })
+
+const Workout = mongoose.model("workout", Class)
 module.exports = Workout;
