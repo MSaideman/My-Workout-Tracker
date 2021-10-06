a8445eccf706d0874bcd27645a1654ef469d6b9e
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-05 21:21:57 -0700

    added model files and route files

---
+++ routes/apiroutes.js
@@ -0,0 +1,10 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    app.get("", (req,res) => {
+    })
+    app.post("", (req,res) => {
+    })
+    app.put("", (req,res) => {
+    })
+}
