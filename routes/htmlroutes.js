a3856e8fb17c1815571b7f320fd0505097ec65ff
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-12 11:12:47 -0500

    added html routes, bug not showing cardio or dashboard info

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 module.exports = function(app) {
 
-    app.get("", function(req, res) {
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
   
-    app.get("", function(req, res) {
+    app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
 };
 
