66df9019dce4acca6ee12d31bab099406b848c5c
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-21 07:00:08 -0700

    add unified topology

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  useFindAndModify: false
 });
 
 // require routes
